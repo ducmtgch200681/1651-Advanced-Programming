f712fb810a0486e1affc0ebcbbd86900da5d275f
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2022-07-19 23:13:05 +0700

    ok

---
+++ Cinema/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            while (true)
+            {
+                PrintMenu();
+                string movie = SelectMovie();
+                if (movie == "") break;
+                int Aticket = GetAdultTicket();
+                int Cticket = GetChildrenTicket();
+                int Total = GetBill(movie, Aticket, Cticket);
+                Pay(Total);
+            }
+            
+        }
+
+        // Menu
+        static void PrintMenu()
+        {   
+            Console.WriteLine("\nWelcome to Duke cinema!");
+            Console.WriteLine("Now Playing: ");
+            Console.WriteLine("1. Thor 4");
+            Console.WriteLine("2. Minion");
+            Console.WriteLine("3. Land of Death");
+            Console.WriteLine("4. Exit!");
+        }
+
+        // selected movie
+        private static string SelectMovie()
+        {
+            bool invalid = true;
+            int option;
+            string movie = "...";
+
+            while (invalid)
+            {
+                Console.WriteLine("Choose the Movie you want");
+                option = int.Parse(Console.ReadLine());
+                invalid = option < 1 || option > 4;
+                if (invalid)
+                    Console.WriteLine("Invalid choice. Please choose again!");
+
+                
+                switch(option)
+                {
+                    case 1: 
+                        movie = "Thor 4";
+                        break;
+                    case 2:
+                        movie = "Minion";
+                        break;
+                    case 3:
+                        movie = "Land of Death";
+                        break;
+                    case 4:
+                        movie = "";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            return movie;
+        }
+
+        // Adult ticket
+        private static int GetAdultTicket()
+        {
+            int Aticket;
+
+            Console.Write("Number of adult ticket ($2): ");
+            Aticket = int.Parse(Console.ReadLine());
+
+            return Aticket * 2;
+        }
+
+        // Children ticket
+        private static int GetChildrenTicket()
+        {
+            int Cticket;
+
+            Console.Write("Number of children ticket ($1): ");
+            Cticket = int.Parse(Console.ReadLine());
+
+            return Cticket;
+        }
+
+        // Bill
+        private static int GetBill(string movie, int Aticket, int Cticket)
+        {
+            int Total;
+            Total = Aticket + Cticket;
+
+            Console.WriteLine("Your bill: ");
+            Console.WriteLine("Movie: " + movie);
+            Console.WriteLine("Adult tickets:    $" + Aticket);
+            Console.WriteLine("Children tickets: $" + Cticket);
+            Console.WriteLine("Total             $" + Total);
+            return Total;
+        }
+
+        // Payment
+        static void Pay(int Total)
+        {
+            Console.Write("Your Payment: ");
+
+
+            bool run = true;
+            while (run)
+            {
+                int money = int.Parse(Console.ReadLine());
+                int change = money - Total;
+
+                if (change > 0){
+                    Console.WriteLine("Here your change: " + change);
+                    Console.WriteLine("Have a nice movie! <3");
+                    run = false;
+                }           
+
+                if (change == 0){
+                    Console.WriteLine("Have a nice movie! <3");
+                    run = false;
+                }
+
+                if (change < 0){
+                    Total = -change;
+                    Console.WriteLine("Infusion amount of money: " + Total);
+                    Console.Write("Please deposit more: ");
+                }
+            }
+        }
+    }
+}
--- Vendingmachine/Program.cs
+++ Vendingmachine/Program.cs
@@ -15,10 +15,12 @@ namespace Vendingmachine
             do
             {
                 Console.WriteLine("\nWelcome to Kevin the vendingmachine! \nPlease choose your drink: \n1. Coke($2) \n2. Water($1) \n3. Tea($1.5) \n4. Exit");
-                Console.WriteLine("\nEnter your choice: ");
-                option = Convert.ToInt32(Console.ReadLine());
+                // Console.WriteLine("\nEnter your choice: ");
+                // option = Convert.ToInt32(Console.ReadLine());
 
-                switch(option)
+                
+
+                switch(GetDrinkChoice())
                 {
                     case 1:
                         Console.WriteLine("Coke is $2");
@@ -26,51 +28,54 @@ namespace Vendingmachine
                         quantity = Convert.ToInt32(Console.ReadLine());
                         bill = quantity*2;
                         Console.WriteLine("Total: " + bill);
-                        Console.WriteLine("Your cash: ");
-                        cash = Convert.ToInt32(Console.ReadLine());
-                        if (cash != 1 && cash !=2 && cash !=5 && cash != 10){
-                            Console.WriteLine("Invalid currency!");
-                            break;
-                        }
-                        if (cash > bill)
-                        {
-                            change = cash - bill;
-                            Console.WriteLine("Here your change: " + change);
-                            Console.WriteLine("Thankyou for purchase! <3");
-                            break;
-                        }
-                        if (cash == bill)
-                        {
-                            Console.WriteLine("Thankyou for purchase! <3");
-                            break;
-                        }
-                        if (cash < bill)
-                        {
-                            for(change = bill - cash; change < bill ; change -= cash){
-                                if (change <= 0){
-                                    Console.WriteLine("Thankyou for purchase! <3");
-                                    break;
-                                }
 
-                                Console.WriteLine("Your bill: " + bill);
-                                Console.WriteLine("Infusion amount of money: " + change);
-                                Console.WriteLine("Deposit money: ");
-                                cash = Convert.ToInt32(Console.ReadLine());
+                        Pay(bill);
 
-                                if (cash != 1 && cash !=2 && cash !=5 && cash != 10){
-                                    Console.WriteLine("Invalid currency!");
-                                    break;
-                                }
+                        // Console.WriteLine("Your cash: ");
+                        // cash = Convert.ToInt32(Console.ReadLine());
+                        // if (cash != 1 && cash !=2 && cash !=5 && cash != 10){
+                        //     Console.WriteLine("Invalid currency!");
+                        //     break;
+                        // }
+                        // if (cash > bill)
+                        // {
+                        //     change = cash - bill;
+                        //     Console.WriteLine("Here your change: " + change);
+                        //     Console.WriteLine("Thankyou for purchase! <3");
+                        //     break;
+                        // }
+                        // if (cash == bill)
+                        // {
+                        //     Console.WriteLine("Thankyou for purchase! <3");
+                        //     break;
+                        // }
+                        // if (cash < bill)
+                        // {
+                        //     for(change = bill - cash; change < bill ; change -= cash){
+                        //         if (change <= 0){
+                        //             Console.WriteLine("Thankyou for purchase! <3");
+                        //             break;
+                        //         }
 
-                                if (cash > change)
-                                {
-                                    change = cash - change;
-                                    Console.WriteLine("Here your change: " + change);
-                                    Console.WriteLine("Thankyou for purchase! <3");
-                                    break;
-                                }
-                            }
-                        }
+                        //         Console.WriteLine("Your bill: " + bill);
+                        //         Console.WriteLine("Infusion amount of money: " + change);
+                        //         Console.WriteLine("Deposit money: ");
+                        //         cash = Convert.ToInt32(Console.ReadLine());
+
+                        //         if (cash != 1 && cash !=2 && cash !=5 && cash != 10){
+                        //             Console.WriteLine("Invalid currency!");
+                        //             break;
+                        //         }
+
+                        //         if (cash > change)
+                        //         {
+                        //             change = cash - change;
+                        //             Console.WriteLine("Here your change: " + change);
+                        //             Console.WriteLine("Thankyou for purchase! <3");
+                        //             break;
+                        //         }
+                        //     }
+                        // }
                         break;
                     case 2:
                         Console.WriteLine("Water is $1");
@@ -180,7 +185,111 @@ namespace Vendingmachine
                         break;
                 }
             }
-            while (!option.Equals(4));
+            while (GetDrinkChoice() != 4);
+
+            
         }
+        // get drink choice
+            static int GetDrinkChoice()
+            {
+                bool invalid = true;
+                int option = 0;
+                while (invalid)
+                {
+                    Console.WriteLine("Enter your choice (1-4): ");
+                    option = int.Parse(Console.ReadLine());
+                    // invalid = choice != 1 && choice != 2 && choice != 3;
+                    invalid = option < 1 || option > 4;
+                    if (invalid)
+                    Console.WriteLine("Invalid choice. Please try again!");
+                }
+                return option;
+            }
+
+            // get number of drinks
+            static int GetNumberOfDrinks()
+            {
+                int quantity;
+                Console.WriteLine("How many you want to buy: ");
+                quantity = int.Parse(Console.ReadLine());
+                return quantity;
+            }
+
+            // cash in and check valid currentcy
+            // static double CashIn()
+            // {
+            //     double cash;
+            //     Console.WriteLine("Your cash: ");
+            //     cash = int.Parse(Console.ReadLine());
+            //     if (cash != 1 && cash !=2 && cash !=5 && cash != 10){
+            //         Console.WriteLine("Invalid currency!");
+            //         break;
+            //     }
+                
+            // }
+
+            // payment
+            static void Pay(double bill)
+            {
+                double cash;
+                double change;
+
+                Console.WriteLine("Your cash: ");
+                cash = int.Parse(Console.ReadLine());
+
+                do{
+                    Console.WriteLine("Invalid currency!");
+                    break;
+                    
+                }while (cash != 1 && cash !=2 && cash !=5 && cash != 10);
+
+                 do
+                    {
+                        change = cash - bill;
+                        Console.WriteLine("Here your change: " + change);
+                        Console.WriteLine("Thankyou for purchase! <3");
+                        break;
+                        
+                    }while(cash > bill);
+                do 
+                    {
+                        Console.WriteLine("Thankyou for purchase! <3");
+                        break;
+                        
+                    }while(cash == bill);
+                do
+                    {
+                        for(change = bill - cash; change < bill ; change -= cash){
+                            do{
+                                // if (change <= 0){
+                                    Console.WriteLine("Thankyou for purchase! <3");
+                                    break;
+                                    
+                                // }
+                            }while(change <= 0);
+                            
+
+                            Console.WriteLine("Your bill: " + bill);
+                            Console.WriteLine("Infusion amount of money: " + change);
+                            Console.WriteLine("Deposit money: ");
+                            cash = Convert.ToInt32(Console.ReadLine());
+
+                            do{
+                                Console.WriteLine("Invalid currency!");
+                                break;
+                                
+                            }while(cash != 1 && cash !=2 && cash !=5 && cash != 10);
+
+                            
+                            do{
+                                change = cash - change;
+                                Console.WriteLine("Here your change: " + change);
+                                Console.WriteLine("Thankyou for purchase! <3");
+                                break;
+                                
+                            }while(cash > change);
+                        }
+                    }while (cash < bill);
+            }
     }
 }
